f4e73f42c0ecf043f254e5aaa8798d16a59ecbbb
Abhrojit Mukherjee <abhrojit.mukherjee1994@gmail.com>
2015-10-31 23:27:12 +0530

    DrawerLayout initiated

---
+++ app/src/main/java/com/mycompany/fragment/Fragment2.java
@@ -0,0 +1,17 @@
+package com.mycompany.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Abhrojit on 9/2/2015.
+ */
+public class Fragment2 extends Fragment {
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+        return inflater.inflate(R.layout.activity_fragment2,container,false);
+    }
+
+}
